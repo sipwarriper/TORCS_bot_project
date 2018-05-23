82057eefbf07fa2e51fc7328adeb3bb3d2090741
marccane <marc00077@hotmail.com>
2018-05-23 17:08:07 +0200

    Molts de canvis

--- src/scr/FuzzyTorcsController.java
+++ src/scr/FuzzyTorcsController.java
@@ -5,19 +5,22 @@ import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.sun.xml.internal.ws.api.message.ExceptionHasMessage;
 import net.sourceforge.jFuzzyLogic.FIS;
 import net.sourceforge.jFuzzyLogic.FunctionBlock;
+import sun.management.Sensor;
 
 public class FuzzyTorcsController extends Controller {
 
-    double distanceFromStart;
+    /*double distanceFromStart;
     final double targetSpeed = 30;
-    final int samplingDistance = 5;
+    final int samplingDistance = 5;*/
     static List<Record> registry = getFromFile("Registre.torc");
 
     public Action control(SensorModel sensorModel) {
-        // Load from 'FCL' file
+
+        Action action = new Action();
+
+        //Inicialitzar fuzzylogic
         String fileName = "fcl/fuzzylogic.fcl";
         FIS fis = FIS.load(fileName,true);
 
@@ -26,8 +29,6 @@ public class FuzzyTorcsController extends Controller {
             return null;
         }
 
-        Action action = new Action();
-
         if(sensorModel.getDistanceRaced()<400){
             action.accelerate=1;
         }
@@ -35,19 +36,32 @@ public class FuzzyTorcsController extends Controller {
             action.accelerate=0;
             action.brake=0.5;
         }
+        
+        action.steering = girar_temporal(sensorModel); //steering temporal sense fuzzy
 
         // Set Variables
         FunctionBlock gearBlock = fis.getFunctionBlock("gear");
         gearBlock.setVariable("rpm", sensorModel.getRPM());
         gearBlock.setVariable("accelerate", action.accelerate);
 
+        /*FunctionBlock acceleration = fis.getFunctionBlock("acceleration");
+        acceleration.setVariable("distNextTurn", );
+        acceleration.setVariable("angle", );
+        acceleration.setVariable("speed", );
+        acceleration.setVariable("currentTurn", );*/
+
         // Evaluate
         gearBlock.evaluate();
-//        fis.evaluate();
 
-        // Show output variable's chart
         int gear = (int) gearBlock.getVariable("outgear").getValue();
-        action.gear = sensorModel.getGear()+gear;
+        /*if(!(sensorModel.getGear() == 1 && gear == -1))*/ 
+        action.gear = sensorModel.getGear() + gear;
+        if(action.gear == 0) action.gear = 1;
+        
+        /*double accel = acceleration.getVariable("accel").getValue();
+        if(accel < 0) action.brake = -accel;
+        else action.accelerate = accel;*/
+
         return action;
     }
 
@@ -64,6 +78,35 @@ public class FuzzyTorcsController extends Controller {
         }
 
         return read_registry;
+    }
+    
+    private double girar_temporal(SensorModel sensorModel){
+        final double targetSteering = 0.25;
+        final double targetTrackPos = 0.1;
+        final double targetSteering2 = 0.2;
+
+        double steering = 0;
+
+        if(sensorModel.getTrackPosition() > targetTrackPos){
+            steering += -targetSteering;
+        }
+        else if(sensorModel.getTrackPosition() < -targetTrackPos){
+            steering += targetSteering;
+        }
+        else{
+            //girem per mantenir-nos paral·lels a la trajectoria de la pista
+            if (sensorModel.getAngleToTrackAxis() < -0.2) {
+                steering += -targetSteering2;
+            }
+            else if(sensorModel.getAngleToTrackAxis() > 0.2){
+                steering += targetSteering2;
+            }
+            else {
+                steering = 0;
+            }
+        }
+
+        return steering;
     }
 
     public void reset() {
